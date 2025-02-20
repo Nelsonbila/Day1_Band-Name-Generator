30cd6f6674f4466a2b5becb2880abe4f61dc55a7
Nelsonbila <137395466+Nelsonbila@users.noreply.github.com>
2025-02-20 13:00:07 +0200

    Add files via upload

---
+++ Day1_Band_Name_Generator.py
@@ -0,0 +1,12 @@
+#Nelson Bila
+#Day1 roject
+#19/02/2025
+
+print("Hi, welcome to the Band Name Generator")
+city = input("What is the name of the city you grew up in?")
+print(city)
+
+pet = input("What is your pet's name?")
+print(pet)
+
+print("Your band name could be" + city + " " + pet)
